d41cc543f1f959ee6a846b6f464fbc228b0a9110
Joel Filipe Martins Amaral <131992612+Kng1011@users.noreply.github.com>
2024-06-20 22:05:44 +0100

    .

--- js/gameScene.js
+++ js/gameScene.js
@@ -1340,9 +1340,9 @@ spawnEnemy(enemyTypeKey) {
         const randomScene = Math.random();
         this.level += 1;
     
-        if (randomScene < 0.5 && this.selectedSkills.length <= 4) {
+        if (randomScene < 0.5 && this.selectedSkills.length < 4) {
             this.scene.start('SkillSelectionScene', { selectedSkills: this.selectedSkills, selectedPowerUps: this.selectedPowerUps, level: this.level });
-        } else if(randomScene >= 0.5 && this.selectedPowerUps.length <= 6) {
+        } else if(randomScene >= 0.5 && this.selectedPowerUps.length < 6) {
             this.scene.start('PowerUpSelectionScene', { selectedSkills: this.selectedSkills, selectedPowerUps: this.selectedPowerUps, level: this.level });
         } else {
             this.scene.start('GameScene', { selectedSkills: this.selectedSkills, selectedPowerUps: this.selectedPowerUps, level: this.level });
